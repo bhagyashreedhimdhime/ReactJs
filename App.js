9966a23e99aa1afa769c2ecfa3d58799c0b9eeee
sanghpal <ssanghpal8@gmail.com>
2025-04-29 16:48:04 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+
 const parent= React.createElement("div", { id: "parent"}, 
     [React.createElement("div", { id: "child 1"}, 
         [React.createElement("h1", { id:"heading"}, "I am child 1 h1 tag"),
